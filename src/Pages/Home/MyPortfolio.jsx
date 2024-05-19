e4f6f77195e99e4f5f20c4690a11932b371fb884
ritiks11 <sharmaritik414@gmail.com>
2024-05-20 01:47:38 +0530

    project changes

--- src/Pages/Home/MyPortfolio.jsx
+++ src/Pages/Home/MyPortfolio.jsx
@@ -13,37 +13,26 @@ export default function MyPortfolio() {
           </h2>
         </div>
       </div>
-      <div className="portfolio--section--container">
-        {data?.portfolio?.map((item, index) => (
-          <div key={index} className="portfolio--section--card">
-            <div className="portfolio--section--img">
-              <img src={item.src} alt="Placeholder" />
-            </div>
-            <div className="portfolio--section--card--content">
-              <div>
-                <h3 className="portfolio--section--title">{item.title}</h3>
-                <p className="text-md">{item.description}</p>
-              </div>
-              <p className="text-sm portfolio--link">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  viewBox="0 0 20 19"
-                  fill="none"
-                >
-                  <path
-                    d="M4.66667 1.66675H18V15.0001M18 1.66675L2 17.6667L18 1.66675Z"
-                    stroke="currentColor"
-                    stroke-width="2.66667"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </svg>
-              </p>
-            </div>
+      <div className="projects__content">
+        <div className="projects_row">
+          <div className="projects__row-img-cont">
+            <img
+              src="./img/bingeflix.png"
+              alt="project screenshot"
+              className="projects__row-img"
+            />
           </div>
-        ))}
+          <div className="projects__row-content">
+            <h3 className="projects__row-content-title">BingeFlix</h3>
+            <p className="projects__row-content-desc">
+              Bingeflix is a dynamic React project designed to immerse users in
+              the world of movies and TV shows. With its intuitive interface,
+              users can effortlessly navigate through a vast database of
+              entertainment content.
+            </p>
+            <a className="btn_to_projectDetails">Case Study</a>
+          </div>
+        </div>
       </div>
     </section>
   );
